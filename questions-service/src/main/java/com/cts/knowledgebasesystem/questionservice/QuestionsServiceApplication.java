1d907d0e80bf5cc20c734f0d7f68f70138b0354c
narendrakl <narendraklr@gmail.com>
2022-05-28 18:55:40 +0530

    Added gateway service and implemented actuator.

--- questions-service/src/main/java/com/cts/knowledgebasesystem/questionservice/QuestionsServiceApplication.java
+++ questions-service/src/main/java/com/cts/knowledgebasesystem/questionservice/QuestionsServiceApplication.java
@@ -2,10 +2,12 @@ package com.cts.knowledgebasesystem.questionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class QuestionsServiceApplication {
 
 	public static void main(String[] args) {
